d04fa368c2062063d55446d2076bdfd5a1bc4196
Jeketos <jeketos@gmail.com>
2023-01-19 13:30:37 +0200

    Prepare for 0.1.0 release

--- sample/src/main/java/com/idapgroup/autosizetext/sample/MainActivity.kt
+++ sample/src/main/java/com/idapgroup/autosizetext/sample/MainActivity.kt
@@ -3,12 +3,9 @@ package com.idapgroup.autosizetext.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -17,6 +14,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.idapgroup.autosizetext.AutoSizeText
 import com.idapgroup.autosizetext.sample.ui.theme.AutoSizeTextTheme
+
+private const val longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer " +
+        "feugiat, ligula a volutpat dictum, velit est cursus erat, non eleifend dui leo " +
+        "nec diam. Vestibulum aliquet accumsan mollis. Praesent pellentesque lacinia " +
+        "pretium. Ut nec convallis tortor. Duis quis venenatis justo. Integer convallis " +
+        "ipsum dolor, in dignissim diam malesuada at. Praesent ac lacus neque. Praesent " +
+        "imperdiet ultricies purus sagittis accumsan. Vivamus dignissim orci ut metus " +
+        "tristique porttitor. In vel auctor ipsum."
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,21 +55,23 @@ fun Greeting() {
             minFontSize = 8.sp
         )
         AutoSizeText(
+            modifier = Modifier.height(100.dp),
             text = longText,
-            maxLines = 4,
             fontSize = 40.sp,
         )
+        AutoSizeText(
+            modifier = Modifier.height(40.dp),
+            text = longText,
+            fontSize = 40.sp,
+        )
+        Box(modifier = Modifier.height(40.dp)) {
+            AutoSizeText(
+                text = longText,
+                fontSize = 40.sp,
+            )
+        }
     }
 }
-
-private val longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer " +
-        "feugiat, ligula a volutpat dictum, velit est cursus erat, non eleifend dui leo " +
-        "nec diam. Vestibulum aliquet accumsan mollis. Praesent pellentesque lacinia " +
-        "pretium. Ut nec convallis tortor. Duis quis venenatis justo. Integer convallis " +
-        "ipsum dolor, in dignissim diam malesuada at. Praesent ac lacus neque. Praesent " +
-        "imperdiet ultricies purus sagittis accumsan. Vivamus dignissim orci ut metus " +
-        "tristique porttitor. In vel auctor ipsum."
-
 
 @Preview(showBackground = true)
 @Composable
